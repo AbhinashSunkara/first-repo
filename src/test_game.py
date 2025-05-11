879ec61fbd050c289518bc8d07074ff500367398
Abhinash Sunkara <abhinashsunkara14@gmail.com>
2025-05-11 21:30:12 +0530

    Added dummy test case

---
+++ src/test_game.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestGame(unittest.TestCase):
+    def test_score(self):
+        # Example test: assert if score increases correctly
+        self.assertEqual(5 + 5, 10)
+
+if __name__ == '__main__':
+    unittest.main()
